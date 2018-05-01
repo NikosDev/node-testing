14e8ee3e0857a7d4bafb2fa8f01b62caec92e61d
Nikos Kanakis <nikk@hotmail.gr>
2018-05-01 15:41:53 +0300

    add new route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,9 +10,9 @@ var app = express();
 
 app.use(bodyParser.json());
 
+
 app.post('/todos', (req,res) => {
-    //console.log(req.body);
-    
+    //console.log(req.body);    
     var todo = new Todo({
         text: req.body.text,
         completed: req.body.completed
@@ -23,6 +24,33 @@ app.post('/todos', (req,res) => {
         res.send(e);
     });
 });
+
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.send(e);
+    });
+});
+
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+
+    if (!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    })
+});
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('App started on port 3000!')
